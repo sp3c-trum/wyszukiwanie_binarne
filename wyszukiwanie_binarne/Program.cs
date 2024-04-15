fdd3e8511f28bd47a771abe21fbb652e086e0aed
Mikołaj Koper <GamingMaster734@gmail.com>
2024-04-15 11:39:45 +0200

    Dodaj pliki projektów.

---
+++ wyszukiwanie_binarne/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] tabela = new int[100];
+
+for (int i = 0; i < tabela.Length; i++)
+{
+    tabela[i] = i*2;
+}
+
+int left = 0;
+int right = tabela.Length - 1;
+int mid;
+int szukana;
+int szukajLiczby()
+{
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+
+        if (tabela[mid] == szukana) {
+            return mid;
+        }
+        else if (tabela[mid] > szukana)
+        {
+            right = mid - 1;
+        } else
+        {
+            left = mid + 1;
+        }
+    }
+    return -1;
+};
+
+Console.WriteLine("Wpisz liczbę któej szukasz: ");
+szukana = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Indeks tej liczby to : " + szukajLiczby());
